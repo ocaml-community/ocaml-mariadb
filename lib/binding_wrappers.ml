7e5d63f84884172aaa9a40f28ffd138d4f1cd7bb
Andre Nathan <andrenth@gmail.com>
2017-12-20 13:49:13 -0200

    Remove duplicate function definition

--- lib/binding_wrappers.ml
+++ lib/binding_wrappers.ml
@@ -1,4 +1,5 @@
 open Ctypes
+open Util
 
 module B = Ffi_bindings.Bindings(Ffi_generated)
 module T = Ffi_bindings.Types(Ffi_generated_types)
@@ -46,20 +47,6 @@ let mysql_stmt_num_rows stmt =
 
 let mysql_stmt_affected_rows stmt =
   Unsigned.ULLong.to_int @@ B.mysql_stmt_affected_rows stmt
-
-let char_ptr_buffer_of_string s =
-  let len = String.length s in
-  let buf = allocate_n char ~count:(len + 1) in
-  for i = 0 to len - 1 do
-    let p = buf +@ i in
-    p <-@ s.[i]
-  done;
-  (buf +@ len) <-@ '\000';
-  buf
-
-let char_ptr_opt_buffer_of_string = function
-  | None -> None
-  | Some s -> Some (char_ptr_buffer_of_string s)
 
 (* Blocking API *)
 
